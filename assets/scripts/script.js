9135430690b90aff9517d14f2c89adbfd0afa970
Suba Shree V S <subashree188@gmail.com>
2021-05-11 22:24:18 +0530

    Add files via upload

---
+++ assets/scripts/script.js
@@ -0,0 +1,7 @@
+<!How to change HTML using JavaScript>
+<p id = "change">
+This Text Changes
+</p>
+<button onclick = 'document.getElementById("change").innerHTML = "Text Changed!"' type 'button'>
+  CLICK ME
+</button>
